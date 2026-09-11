d4eb070a5cef1a67f60756227021070ca37d66a8
mirzoyantigran61-cell <mirzoyantigran61@gmail.com>
2026-09-12 00:01:26 +0400

    Add async_mode to SocketIO initialization

--- extensions.py
+++ extensions.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+
 from flask_socketio import SocketIO
+
 
 socketio = SocketIO(
     cors_allowed_origins="*",
+    async_mode="threading",
     logger=False,
     engineio_logger=False,
 )
